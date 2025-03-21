ab9c704b53eb2e130b7f9ba5839eaa0fa6a48ade
silen7-pool <mo7trading@gmail.com>
2025-03-21 12:53:05 +0000

    adding contrast variant on button

--- src/dashboard content header/DashboardContentHeader.tsx
+++ src/dashboard content header/DashboardContentHeader.tsx
@@ -1,14 +1,14 @@
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Calendar } from "lucide-react";
 import { Select } from "../components/Select";
 import { Button } from "../components/ui/button";
-import { Tabs, TabsList, TabsTrigger } from "../components/ui/tabs";
 
 export const DashboardContentHeader = () => {
   return (
     <div className="mt-8 flex flex-col gap-6 pr-8 pl-4 lg:mt-12">
       <div className="flex w-full items-center gap-4">
         <h1 className="mr-auto px-4 text-3xl font-bold">Dashboard</h1>
-        <Select>
+        <Select className="h-9">
           <div className="flex items-center gap-4">
             <Calendar size={20} />
             <span className="text-sm">Mar 18, 2025 - Apr 2, 2025</span>
